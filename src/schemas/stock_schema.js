860bc6559214519c4af2849d22cdfe8761287c8e
Agustín <agustinlez26@gmail.com>
2026-01-28 14:59:48 -0300

    feat: add stock validations

---
+++ src/schemas/stock_schema.js
@@ -0,0 +1,54 @@
+import { z } from 'zod'
+import { idSchema } from './shared_schema.js'
+
+const stock_schema = z.object({
+    product_id: idSchema,
+    branch_id: idSchema,
+    quantity: z.coerce.number().int().min(0),
+    min_quantity: z.coerce.number().int().min(0),
+})
+
+const update_stock_schema = stock_schema.pick({
+    quantity: true,
+    min_quantity: true
+}).partial()
+
+const params = z.object({
+    search: z.string().optional(),
+    category_id: z.coerce.number().int().positive().optional(),
+    branch_id: z.coerce.number().int().positive().optional(),
+    low_stock: z.string().transform(val => val === 'true').optional(),
+    out_stock: z.string().transform(val => val === 'true').optional(),
+    page: z.coerce.number().int().min(1).optional().default(1)
+}).transform((data) => {
+    return {
+        search: data.search,
+        category: data.category_id,
+        branch: data.branch_id,
+        low_stock: data.low_stock,
+        out_stock: data.out_stock,
+        page: data.page
+    }
+})
+
+/**
+ * Tipo para crear stock (Assign)
+ * @typedef {z.infer<typeof stock_schema>} StockDTO
+ */
+
+/**
+ * Tipo para los filtros de búsqueda
+ * @typedef {z.infer<typeof params>} StockParamsDTO
+ */
+
+export function validateStock(input) {
+    return stock_schema.safeParse(input)
+}
+
+export function validateUpdateStock(input) {
+    return update_stock_schema.safeParse(input)
+}
+
+export function validateParams(input) {
+    return params.safeParse(input)
+}
