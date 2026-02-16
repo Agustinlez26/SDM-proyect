6e842d1ffa16a198acf6f824f711136e0839bb78
Agustín <agustinlez26@gmail.com>
2026-02-16 09:21:03 -0300

    fix: configuring app.js for reading json and rendering ejs

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+import 'dotenv/config'
 import express from 'express'
+import routes from './routes/index.js'
+import path from 'path'
+import cookieParser from 'cookie-parser'
 const PORT = process.env.PORT ?? 1234
 const app = express()
 
 app.disable('x-powered-by')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use('/', routes)
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(process.cwd(), 'src', 'public', 'views'))
+app.use(express.static('src/public'))
 
 app.listen(PORT, () => {
     console.log(`App listen in http://localhost:${PORT}`)
